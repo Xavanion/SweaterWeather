80c7f64177fd183fabee1204a72b6cda2b6123f7
Barrett Brown <barrettbrown2012@gmail.com>
2023-04-14 23:41:51 -0500

    Removed a lot of stoof

--- main.py
+++ main.py
@@ -14,46 +14,32 @@ cur_location = ','.join(cur_location)
 # Configure API key authorization: ApiKeyAuth
 configuration = swagger_client.Configuration()
 configuration.api_key['key'] = ''
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['key'] = 'Bearer'
 
 # create an instance of the API class
 api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
 q = cur_location # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more. 
-days = 56 # int | Number of days of weather forecast. Value ranges from 1 to 14
-dt = '2013-10-20' # date | Date should be between today and next 14 day in yyyy-MM-dd format. e.g. '2015-01-01'  (optional)
-unixdt = 56 # int | Please either pass 'dt' or 'unixdt' and not both in same request. unixdt should be between today and next 14 day in Unix format. e.g. 1490227200  (optional)
-hour = 56 # int | Must be in 24 hour. For example 5 pm should be hour=17, 6 am as hour=6  (optional)
-lang = 'lang_example' # str | Returns 'condition:text' field in API in the desired language.<br /> Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to check 'lang-code'.  (optional)
+days = 2 # int | Number of days of weather forecast. Value ranges from 1 to 14
+dt = '2023-06-20' # date | Date should be between today and next 14 day in yyyy-MM-dd format. e.g. '2015-01-01'  (optional)
+hour = 12 # int | Must be in 24 hour. For example 5 pm should be hour=17, 6 am as hour=6  (optional)
+
 
 try:
     # Forecast API
-    api_response = api_instance.forecast_weather(q, days, dt=dt, unixdt=unixdt, hour=hour, lang=lang)
+    api_response = api_instance.forecast_weather(q, days, dt=dt, hour=hour)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling APIsApi->forecast_weather: %s\n" % e)
 
-
-'''
-# create an instance of the API class
-api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
-q = 'q_example' # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more. 
-dt = '2013-10-20' # date | Date should be between 14 days and 300 days from today in the future in yyyy-MM-dd format (i.e. dt=2023-01-01)  (optional)
-lang = 'lang_example' # str | Returns 'condition:text' field in API in the desired language.<br /> Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to check 'lang-code'.  (optional)
-
+    
 try:
-    # Future API
-    api_response = api_instance.future_weather(q, dt=dt, lang=lang)
+    # Future API dt is 14-300 days in the future
+    api_response = api_instance.future_weather(q, dt=dt)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling APIsApi->future_weather: %s\n" % e)
 
-# Configure API key authorization: ApiKeyAuth
-configuration = swagger_client.Configuration()
-configuration.api_key['key'] = 'YOUR_API_KEY'
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['key'] = 'Bearer'
 
+'''
 # create an instance of the API class
 api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
 q = 'q_example' # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more. 
@@ -71,13 +57,6 @@ try:
 except ApiException as e:
     print("Exception when calling APIsApi->history_weather: %s\n" % e)
 
-
-# Configure API key authorization: ApiKeyAuth
-configuration = swagger_client.Configuration()
-configuration.api_key['key'] = ''
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['key'] = 'Bearer'
-
 # create an instance of the API class
 api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
 q = 'q_example' # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more. 
@@ -89,12 +68,6 @@ try:
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling APIsApi->realtime_weather: %s\n" % e)
-
-# Configure API key authorization: ApiKeyAuth
-configuration = swagger_client.Configuration()
-configuration.api_key['key'] = ''
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['key'] = 'Bearer'
 
 # create an instance of the API class
 api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
