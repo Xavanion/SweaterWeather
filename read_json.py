8485a2cb7ccf3c43853c844394cd3dfb84e2e3c7
minhvuw <minhtvuw@gmail.com>
2023-04-15 19:34:16 -0500

    history data is good now

--- read_json.py
+++ read_json.py
@@ -5,7 +5,34 @@ class Read:
         self.file = file_name
 
     def history(self):
-        pass
+        location_data = {}
+        location_data['country'] = json_data['location']['country']
+        location_data['city'] = json_data['location']['name']
+
+        monthly_data_json = json_data['forecast']['forecastday']
+        monthly_data = {}
+        i = 1
+        for forecastday in monthly_data_json:
+            date = forecastday['_date']
+
+            tempinfo = {}
+            tempinfo['mintemp'] = forecastday['day']['mintemp_f']
+            tempinfo['maxtemp'] = forecastday['day']['maxtemp_f']
+            tempinfo['avgtemp'] = forecastday['day']['avgtemp_f']
+
+            conditioninfo = {}
+            conditioninfo['icon'] = forecastday['day']['condition']['icon']
+            conditioninfo['condition'] = forecastday['day']['condition']['text']
+
+            dayinfo = {}
+            dayinfo['date'] = date
+            dayinfo['temperatures'] = tempinfo
+            dayinfo['condition'] = conditioninfo
+
+            monthly_data[f'Day {i}'] = dayinfo
+            i += 1
+        return monthly_data     
+        
 
     def run(self):
         global json_data
@@ -13,5 +40,6 @@ class Read:
             json_data = json.load(f)
         
 
-
 read_stuff = Read('history.json')
+
+read_stuff.run()
