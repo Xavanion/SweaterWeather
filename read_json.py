eff23f3282eae0e5c7d1d39f1b98fa33e93df666
minhvuw <minhtvuw@gmail.com>
2023-04-15 20:43:44 -0500

    forecast.json is good to go

--- read_json.py
+++ read_json.py
@@ -11,6 +11,7 @@ class Read:
 
         monthly_data_json = json_data['forecast']['forecastday']
         monthly_data = {}
+        monthly_data['location'] = location_data
         i = 1
         for forecastday in monthly_data_json:
             date = forecastday['_date']
@@ -24,12 +25,12 @@ class Read:
             conditioninfo['icon'] = forecastday['day']['condition']['icon']
             conditioninfo['condition'] = forecastday['day']['condition']['text']
 
-            dayinfo = {}
-            dayinfo['date'] = date
-            dayinfo['temperature'] = tempinfo
-            dayinfo['condition'] = conditioninfo
+            hourinfo = {}
+            hourinfo['date'] = date
+            hourinfo['temperature'] = tempinfo
+            hourinfo['condition'] = conditioninfo
 
-            monthly_data[f'Day {i}'] = dayinfo
+            monthly_data[f'Day {i}'] = hourinfo
             i += 1
         return monthly_data
 
@@ -39,6 +40,7 @@ class Read:
         location_data = {}
         location_data['country'] = json_data['location']['country']
         location_data['city'] = json_data['location']['name']
+        realtimedata['location'] = location_data
 
         currentdata = json_data['current']
         
@@ -52,14 +54,55 @@ class Read:
         tempinfo['feelslike'] = currentdata['feelslike_f']
         realtimedata['temperature'] = tempinfo
 
-        print(realtimedata)
+        return realtimedata 
+    
+    def forecast(self):
+        daily_forecast = {}
+        alldayforecast = json_data['forecast']['forecastday'][0]['hour']
+        date = json_data['forecast']['forecastday'][0]['_date']
+
+        location_data = {}
+        location_data['country'] = json_data['location']['country']
+        location_data['city'] = json_data['location']['name']
+        daily_forecast['location'] = location_data
+
+        i = 1
+        for hourlydata in alldayforecast:
+            times = ["00:00", "06:00", "12:00", "18:00", "23:00"]
+            time = str(hourlydata['time'][11:])
+            if time in times:
+                hourinfo = {}
+
+                dateinfo = {}
+                dateinfo['date'] = date
+                dateinfo['time'] = time
+                hourinfo['date_and_time'] = dateinfo
+
+                info = {}
+                info['temperature'] = hourlydata['temp_f']
+                info['feelslike'] = hourlydata['feelslike_f']
+                info['wind_speed'] = hourlydata['wind_mph']
+                info['chance_of_rain'] = hourlydata['chance_of_rain']
+                info['chance_of_snow'] = hourlydata['chance_of_snow']
+
+                
+                conditioninfo = {}
+                conditioninfo['icon'] = hourlydata['condition']['icon']
+                conditioninfo['condition'] = hourlydata['condition']['text']
+
+                hourinfo['info'] = info
+                hourinfo['conditioninfo'] = conditioninfo
+            daily_forecast[f'time_interval{i}'] = hourinfo
+            i += 1
+        return daily_forecast
+        
 
     def run(self):
         global json_data
         with open(self.file) as f:
             json_data = json.load(f)
-        self.realtime()
+        print(self.forecast())
 
-read_stuff = Read('real_time.json')
 
+read_stuff = Read('forecast.json')
 read_stuff.run()
